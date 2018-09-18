7831a90417e0d629c71dbef7575013a7e8026bb6
David Wade <DAWAD@statoil.com>
2018-09-18 16:30:31 +0200

    Plot confusion matrix

--- src/random_forest.py
+++ src/random_forest.py
@@ -1,29 +1,23 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("../../output/test.csv")
 data["code"] = pd.factorize(data['label'])[0]
 data['is_train'] = np.random.uniform(0, 1, len(data)) <= 0.5
 train, test = data[data['is_train'] == True], data[data['is_train'] == False]
 
-print(test.head())
-
 features = data.columns[1:-4]
-print(features)
-
-y = train['code']
-
-print(y)
 
 clf = RandomForestClassifier(n_jobs=2, random_state=0)
-clf.fit(train[features], y)
-
+clf.fit(train[features], train['code'])
 preds = clf.predict(test[features])
-print(preds)
 
-y_test = test['code']
-
-cm = pd.crosstab(y_test, preds, rownames=['Actual Period'], colnames=['Predicted Period'])
-
+cm = confusion_matrix(test['code'], preds, labels=None, sample_weight=None)
 print(cm)
+
+
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+plt.savefig("../../output/cm.png")
