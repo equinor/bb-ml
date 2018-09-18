b38c1554bad1b42a4befd3cdf841206824dd6dd0
Bjorn Harald Fotland <BFotland@slb.com>
2018-09-18 13:11:22 +0200

    Check all pairs

--- src/species_analyzer.py
+++ src/species_analyzer.py
@@ -17,9 +17,16 @@ class SpeciesAnalyzer:
             new_dict.setdefault(value, set()).add(key)
         return [{key: values} for key, values in new_dict.items() if len(values) > 1]
 
-    def edit_distance_duplicates(self, max_edit_distance=3):
+    def edit_distance_duplicates(self, max_edit_distance=1):
         s = self._species
-        return [{a: s[a], b:s[b]} for a, b in self._pairwise(self._species) if editdistance.eval(s[a], s[b]) < max_edit_distance]
+        duplicates = []
+        for key in s:
+            for key2 in s:
+                if key == key2:
+                    continue
+                if editdistance.eval(s[key], s[key2]) < max_edit_distance:
+                    duplicates.append({key:s[key], key2:s[key2]})
+        return duplicates
 
 
 def run(filenames: List[str]):
@@ -29,12 +36,13 @@ def run(filenames: List[str]):
         species = get_species_from_dex(filename)
         analyzer = SpeciesAnalyzer(species)
 
-        print(str.format("Edit distance matches: {}", analyzer.edit_distance_duplicates()))
+        print(str.format("Edit distance matches: {}",
+                         analyzer.edit_distance_duplicates()))
         all_duplicates.extend(analyzer.find_duplicate_species())
         all_species = {**all_species, ** species}
     all_analyzer = SpeciesAnalyzer(all_species)
     all_analyzer.find_duplicate_species()
-    
+
     print(str.format("Exact matches: {}", all_duplicates))
 
 
