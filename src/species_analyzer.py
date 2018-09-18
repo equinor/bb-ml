df34e1edc173c2bc986a4b3f1a001364ba36bb90
Bjorn Harald Fotland <BFotland@slb.com>
2018-09-18 12:04:27 +0200

    Add edit distance to identify near duplicates.

--- src/species_analyzer.py
+++ src/species_analyzer.py
@@ -1,16 +1,25 @@
 from typing import List
 from raw_dex_reader import get_species_from_dex
+import editdistance
 
 
 class SpeciesAnalyzer:
     def __init__(self, species: dict):
         self._species = species
 
+    def _pairwise(self, iterable):
+        a = iter(iterable)
+        return zip(a, a)
+
     def find_duplicate_species(self):
         new_dict = {}
         for key, value in self._species.items():
             new_dict.setdefault(value, set()).add(key)
         return [{key: values} for key, values in new_dict.items() if len(values) > 1]
+
+    def edit_distance_duplicates(self, max_edit_distance=3):
+        s = self._species
+        return [{a: s[a], b:s[b]} for a, b in self._pairwise(self._species) if editdistance.eval(s[a], s[b]) < max_edit_distance]
 
 
 def run(filenames: List[str]):
@@ -19,11 +28,14 @@ def run(filenames: List[str]):
     for filename in filenames:
         species = get_species_from_dex(filename)
         analyzer = SpeciesAnalyzer(species)
+
+        print(str.format("Edit distance matches: {}", analyzer.edit_distance_duplicates()))
         all_duplicates.extend(analyzer.find_duplicate_species())
         all_species = {**all_species, ** species}
-    analyzer = SpeciesAnalyzer(all_species)
-    analyzer.find_duplicate_species()
-    print(all_duplicates)
+    all_analyzer = SpeciesAnalyzer(all_species)
+    all_analyzer.find_duplicate_species()
+    
+    print(str.format("Exact matches: {}", all_duplicates))
 
 
 if __name__ == "__main__":
